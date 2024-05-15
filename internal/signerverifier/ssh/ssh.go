e8e2d67f95a0b92cce587a06daf4e74b38b170fc
Lukas Puehringer <lukas.puehringer@nyu.edu>
2024-05-15 12:26:45 +0200

    Skip detour over ssh agent
    Signed-off-by: Lukas Puehringer <lukas.puehringer@nyu.edu>

--- internal/signerverifier/ssh/ssh.go
+++ internal/signerverifier/ssh/ssh.go
@@ -2,43 +2,23 @@
 package ssh
 
 import (
-	"crypto/rsa"
+	"crypto/rand"
 	"fmt"
 	"log"
-	"net"
-	"os"
 
 	testdata "github.com/gittuf/gittuf/internal/testartifacts"
 	"golang.org/x/crypto/ssh"
-	"golang.org/x/crypto/ssh/agent"
 )
 
 func SSHKeygen() {
-	priv, err := ssh.ParseRawPrivateKeyWithPassphrase(
+	signer, err := ssh.ParsePrivateKeyWithPassphrase(
 		testdata.SSHRSAPrivateEnc, []byte("hunter2"))
 	if err != nil {
 		log.Fatalf("Failed to parse private key: %s", err)
 	}
 
-	// ssh-agent(1) provides a UNIX socket at $SSH_AUTH_SOCK.
-	socket := os.Getenv("SSH_AUTH_SOCK")
-	conn, err := net.Dial("unix", socket)
-	if err != nil {
-		log.Fatalf("Failed to open SSH_AUTH_SOCK: %v", err)
-	}
-
-	client := agent.NewClient(conn)
-
-	if err = client.Add(agent.AddedKey{PrivateKey: priv}); err != nil {
-		log.Fatalf("Failed to add private key: %s", err)
-	}
-
-	privRsa, _ := priv.(*rsa.PrivateKey)
-	sshPub, _ := ssh.NewPublicKey(&privRsa.PublicKey)
-
 	data := []byte("DATA")
-	sig, err := client.Sign(sshPub, data)
-
+	sig, err := signer.Sign(rand.Reader, data)
 	if err != nil {
 		log.Fatalf("Failed to sign: %s", err)
 	}
