ca62f9ba584ab4ce62a1e698459f2ed6211f8798
Lukas Puehringer <lukas.puehringer@nyu.edu>
2024-05-16 15:09:18 +0200

    Change ssh signing strategy (HEAVY WIP)
    We do not talk to agent direclty, or import private keys,
    instead we just shell out to ssh-keygen, which is exactly what git does.
    
    This commit only adds scaffolding with comments
    
    Signed-off-by: Lukas Puehringer <lukas.puehringer@nyu.edu>

--- internal/signerverifier/ssh/ssh.go
+++ internal/signerverifier/ssh/ssh.go
@@ -2,27 +2,72 @@
 package ssh
 
 import (
-	"crypto/rand"
-	"fmt"
-	"log"
-
-	testdata "github.com/gittuf/gittuf/internal/testartifacts"
-	"golang.org/x/crypto/ssh"
+	"context"
+	"crypto"
 )
 
-func SSHKeygen() {
-	signer, err := ssh.ParsePrivateKeyWithPassphrase(
-		testdata.SSHRSAPrivateEnc, []byte("hunter2"))
-	if err != nil {
-		log.Fatalf("Failed to parse private key: %s", err)
-	}
+// dsse.Verifier interface implementation to be used with ssh Signer
+// TODO: Check ssh signature format. If it isn't different from other rsa,
+// ecdsa, ed25519 signature formats, we don't need a custom ssh Verifier.
 
-	data := []byte("DATA")
-	sig, err := signer.Sign(rand.Reader, data)
-	if err != nil {
-		log.Fatalf("Failed to sign: %s", err)
-	}
+// TODO: make sure this can be added to tuf metadata
+type Verifier struct {
+	keyID  string
+	public *crypto.PublicKey
+}
 
-	fmt.Println(sig)
+func (v *Verifier) Verify(ctx context.Context, data []byte, sig []byte) error {
+	// Verify ssh signature byte stream using crypto stdlib
+}
+func (v *Verifier) KeyID() (string, error) {
+	return v.keyID, nil
+}
+func (v *Verifier) Public() crypto.PublicKey {
+	return s.public
+}
 
+// dsse.Signer interface implementation for ssh key signing
+// Includes verififier to have a single source of truth about signing scheme
+// and keyid
+// Uses ssh-keygen to sign with key from path
+type Signer struct {
+	verifier *Verifier
+	path     string
+}
+
+func (s *Signer) Sign(ctx context.Context, data []byte) ([]byte, error) {
+	// Call ssh-keygen command to create signature
+	// 		ssh-keygen -f <s.path> -y sign
+
+	// Handle errors (key not found, etc.)
+
+	// Parse signature and return bytes
+
+}
+func (s *Signer) KeyID() (string, error) {
+	return s.verifier.keyID, nil
+}
+
+// Import Verifier from path using ssh-keygen.
+// Path can be public or private encrypted or plain key, much like git's
+// user.signingKey config
+// Currently only rsa is supported
+func Import(path string) (*Verifier, error) {
+	// Call ssh-keygen cmd to get x509/SPKI key data
+	// 		ssh-keygen -f <path> -m pkcs8 -e
+
+	// NOTE: `-m pkcs8` is only supported in the latest openssh versions for
+	// ed25519 keys
+
+	// Parse key data into crypto.PublicKey, if key type supported,
+	// and assign to veriifer
+
+	// Assign scheme for keytype
+	// scheme must match scheme used by `ssh-keygen -Y sign`!!
+
+	// Call ssh-keygen cmd to create a fingerprint
+	// and assign to verifier
+	//      ssh-kegen -f <path> -l
+
+	// Return verifier
 }
