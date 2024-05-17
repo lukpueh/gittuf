aee685b011e9bcd83988323df49b2ad56b3b08d9
Lukas Puehringer <lukas.puehringer@nyu.edu>
2024-05-17 13:31:42 +0200

    Remove runCommand helper
    it's not net helpful
    
    Signed-off-by: Lukas Puehringer <lukas.puehringer@nyu.edu>

--- internal/signerverifier/ssh/ssh.go
+++ internal/signerverifier/ssh/ssh.go
@@ -58,13 +58,6 @@ func (s *Signer) KeyID() (string, error) {
 	return s.verifier.keyID, nil
 }
 
-func runCommand(command string) ([]byte, error) {
-	parts := strings.Fields(command)
-	cmd := exec.Command(parts[0], parts[1:]...)
-	return cmd.Output()
-
-}
-
 // Import Verifier from path using ssh-keygen
 //   - Path can be public or private encrypted or plain key, much like git's
 //     user.signingKey config
@@ -73,10 +66,10 @@ func Import(path string) (*Verifier, error) {
 	// Call ssh-keygen cmd to get x509/SPKI key data
 	// NOTE: `-m pkcs8` is only supported in the latest openssh versions for
 	// ed25519 keys
-	command := "ssh-keygen -m pkcs8 -e -f " + path
-	output, err := runCommand(command)
+	cmd := exec.Command("ssh-keygen", "-m", "pkcs8", "-e", "-f", path)
+	output, err := cmd.Output()
 	if err != nil {
-		return nil, fmt.Errorf("failed to run command %v: %v", command, err)
+		return nil, fmt.Errorf("failed to run command %v: %v", cmd, err)
 	}
 
 	block, _ := pem.Decode(output)
@@ -100,10 +93,10 @@ func Import(path string) (*Verifier, error) {
 	}
 
 	// Call ssh-keygen cmd to create a fingerprint to be used as keyid
-	command = "ssh-keygen -l -f " + path
-	output, err = runCommand(command)
+	cmd = exec.Command("ssh-keygen", "-l", "-f", path)
+	output, err = cmd.Output()
 	if err != nil {
-		return nil, fmt.Errorf("failed to run command %v: %v", command, err)
+		return nil, fmt.Errorf("failed to run command %v: %v", cmd, err)
 	}
 
 	trimmed := strings.TrimSpace(string(output[:]))
