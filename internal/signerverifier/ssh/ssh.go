3fd95f7cdccb200a2b09471345a82605eb282091
Lukas Puehringer <lukas.puehringer@nyu.edu>
2024-05-17 13:14:04 +0200

    Implement working ssh Verifier import
    Uses `ssh-keygen -e` command and parses stdout.
    
    Supports import from public, plain private and encrypted private key
    (does not require a password). Currently, only rsa, but this can be
    easily changed.
    
    Signed-off-by: Lukas Puehringer <lukas.puehringer@nyu.edu>

--- internal/signerverifier/ssh/ssh.go
+++ internal/signerverifier/ssh/ssh.go
@@ -4,6 +4,12 @@ package ssh
 import (
 	"context"
 	"crypto"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"os/exec"
+	"strings"
 )
 
 // dsse.Verifier interface implementation to be used with ssh Signer
@@ -13,17 +19,18 @@ import (
 // TODO: make sure this can be added to tuf metadata
 type Verifier struct {
 	keyID  string
-	public *crypto.PublicKey
+	public crypto.PublicKey
 }
 
 func (v *Verifier) Verify(ctx context.Context, data []byte, sig []byte) error {
 	// Verify ssh signature byte stream using crypto stdlib
+	return nil
 }
 func (v *Verifier) KeyID() (string, error) {
 	return v.keyID, nil
 }
 func (v *Verifier) Public() crypto.PublicKey {
-	return s.public
+	return v.public
 }
 
 // dsse.Signer interface implementation for ssh key signing
@@ -41,33 +48,74 @@ func (s *Signer) Sign(ctx context.Context, data []byte) ([]byte, error) {
 
 	// Handle errors (key not found, etc.)
 
+	sig := []byte("")
+
 	// Parse signature and return bytes
+	return sig, nil
 
 }
 func (s *Signer) KeyID() (string, error) {
 	return s.verifier.keyID, nil
 }
 
-// Import Verifier from path using ssh-keygen.
-// Path can be public or private encrypted or plain key, much like git's
-// user.signingKey config
-// Currently only rsa is supported
+func runCommand(command string) ([]byte, error) {
+	parts := strings.Fields(command)
+	cmd := exec.Command(parts[0], parts[1:]...)
+	return cmd.Output()
+
+}
+
+// Import Verifier from path using ssh-keygen
+//   - Path can be public or private encrypted or plain key, much like git's
+//     user.signingKey config
+//   - Currently only rsa is supported
 func Import(path string) (*Verifier, error) {
 	// Call ssh-keygen cmd to get x509/SPKI key data
-	// 		ssh-keygen -f <path> -m pkcs8 -e
-
 	// NOTE: `-m pkcs8` is only supported in the latest openssh versions for
 	// ed25519 keys
+	command := "ssh-keygen -m pkcs8 -e -f " + path
+	output, err := runCommand(command)
+	if err != nil {
+		return nil, fmt.Errorf("failed to run command %v: %v", command, err)
+	}
 
-	// Parse key data into crypto.PublicKey, if key type supported,
-	// and assign to veriifer
+	block, _ := pem.Decode(output)
+	if block == nil {
+		return nil, fmt.Errorf("failed to run decode pem")
+	}
 
-	// Assign scheme for keytype
-	// scheme must match scheme used by `ssh-keygen -Y sign`!!
+	pub, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse public key: %v", err)
+	}
 
-	// Call ssh-keygen cmd to create a fingerprint
-	// and assign to verifier
-	//      ssh-kegen -f <path> -l
+	switch k := pub.(type) {
+	case *rsa.PublicKey:
+		fmt.Println("success!")
+		// TODO: Create SSlibKey, assign scheme for keytype
+		// scheme must match scheme used by `ssh-keygen -Y sign`!!
 
-	// Return verifier
+	default:
+		return nil, fmt.Errorf("unsupported key type: %T", k)
+	}
+
+	// Call ssh-keygen cmd to create a fingerprint to be used as keyid
+	command = "ssh-keygen -l -f " + path
+	output, err = runCommand(command)
+	if err != nil {
+		return nil, fmt.Errorf("failed to run command %v: %v", command, err)
+	}
+
+	trimmed := strings.TrimSpace(string(output[:]))
+	parts := strings.Split(trimmed, " ")
+	if len(parts) < 2 {
+		return nil, fmt.Errorf("unexepcted key fingerpring: %v", trimmed)
+	}
+	keyid := parts[1]
+
+	return &Verifier{
+		keyID:  keyid,
+		public: pub.(*rsa.PublicKey),
+	}, nil
+
 }
--- internal/signerverifier/ssh/ssh_test.go
+++ internal/signerverifier/ssh/ssh_test.go
@@ -1,10 +1,43 @@
 package ssh
 
 import (
+	"path"
+	"path/filepath"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestSign(t *testing.T) {
-	SSHKeygen()
+// FIXME: There must be a more idomatic way to do this, like using go:embed?
+func keyPath(name string) string {
+	dir, _ := filepath.Abs("../../testartifacts/testdata/keys/ssh")
+	return path.Join(dir, name)
+}
 
+// Basic test to import the same rsa verifier from ssh public key and
+// plaintext and encrypted private key (no password needed).
+func TestImport(t *testing.T) {
+
+	tests := []struct {
+		keyName string
+	}{
+		{"rsa"},
+		{"rsa_enc"},
+		{"rsa.pub"},
+	}
+
+	for _, test := range tests {
+		t.Run(test.keyName, func(t *testing.T) {
+			path := keyPath(test.keyName)
+			verifier, err := Import(path)
+			if err != nil {
+				t.Fatalf("Import(%s) error: %v", test.keyName, err)
+			}
+			assert.Equal(t,
+				verifier.keyID,
+				"SHA256:ESJezAOo+BsiEpddzRXS6+wtF16FID4NCd+3gj96rFo",
+			)
+		})
+
+	}
 }
